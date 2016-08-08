7ec04842ac1ee06970ba1ffb2c1013bdf568bd50
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2016-08-08 08:09:06 +0100

    added work with jon

---
+++ syngen.rb
@@ -0,0 +1,245 @@
+# PLOT
+#
+# 1. {life turned upsidedown} [character] ([actor]) has their life turned upsidedown when they discover that their [object] is [surprising thing].  [condition] ensues.
+#
+# 2. {emotional journey} [actor] [impresses] in this portrayal of one man's emotional journey as he struggles to come to terms with the [termstruggler].
+#
+# 3. {a meets b} [a meeting adjective] meeting between [character] ([actor]) and [character] ([actor]) quickly escalates into one of the greatest romances imaginable.  What follows is [love following].
+#
+# 4. {biopic} The [adjective] story of the iconic [time period] [profession] [firstname] [surname] ([actor]) offers a glimpse into the very soul of [place].
+#
+# 5. {documentary} The untold story of the [event] that shook the world.  The film makers were granted unprecedented access to create this [dochype].
+#
+#
+# GUFF
+#
+# 1. this [guffadjective] [guffnoun] from [director adjective] [director] will [guffperbole].
+#
+#
+#
+# TECHNICAL STUFF
+#
+# Presented in [random number between 8 and 180]mm at selected [sites]
+#
+# Rated [rating] for [ratestrength] [ratecontent].
+#
+# [random number between 3 and 607] minutes
+#
+# [one time in ten it says "3 stars, some funny bits"]
+#
+#
+# SUBBIES
+#
+# character:
+#
+# The King's janitor
+# A schoolteacher
+# A union baron
+# A stowaway
+# A local artist in residence
+# A down-on-their-luck lowlife
+#
+# actor:
+# Toby Young
+# Toby Jones
+# Michael Fassbender
+# Benedict Cumberbacth
+# Alicia Vikander
+# Scarlett Johannson
+#
+# object:
+# plunger
+# cucumber
+# mother
+# head of state
+# local mechanic
+# garden shed
+#
+# surprising thing:
+# a portal to another dimension
+# the magical sword excalibur
+# God incarnate
+# their boss' daughter, cursed by a witch
+#
+# condition:
+# Mayhem
+# Chaos
+# Hilarity
+# A lovely time
+# A horny mess
+#
+# impresses:
+# dazzles
+# stuns
+# shines
+# effervesces
+# delights
+# shocks the critics
+# delivers
+#
+# termstruggler:
+# the size of his butte
+# the dissolution of the monastries under Henry VIII
+# the death of his goldfish
+# a carcrash that left him fighting for his life.
+# the enormity of existence
+#
+# a meeting adjective:
+# An innocent
+# A surprise
+# A lunch
+# An unexpected
+#
+# love following:
+# a love story for the ages
+# a paen to the golden age of cinema
+# a love letter to the city of New York
+#
+# adjective:
+# uplifting
+# heartfelt
+# though-provoking
+# mesmeric
+# eye-opening
+#
+#
+# profession:
+# sculptor
+# boxer
+# actor
+# gener
+# tinker
+# tailor
+# soldier
+# spy
+#
+# time period:
+# 18th century
+# 1960s
+# turn of the century
+# victorian
+# jazz-era
+#
+# first name:
+# vincent
+# jonny
+# christopher
+# sophie
+# selina
+#
+# surname:
+# briggs
+# bloggs
+# wood
+# mackintosh
+# alcock
+#
+# place:
+# the industrial northwest of england
+# New York
+# Sub-saharan africa
+# Western Australia
+# French Guyana
+#
+# event:
+# Nicaraguan corn farming blight
+# financial collapse
+# celebrity sex scandal
+# sports victory
+#
+# director:
+# Michael Moore
+# Morgan Spurlock
+# Martin Scorcese
+# Clint Eastwood
+# Quentine Tarantino
+# Sophia Copolla
+# Kathleen Bigelow
+# Lars von Trier
+#
+#
+# dochype:
+# unflinching examination of the human condition
+# testimony to the power of bearing witness
+# monument of modern understanding
+#
+# guffadjective:
+# riproaring
+# searing
+# award winning
+# visceral
+# palm d'or nominated
+# whipsmart
+# quirky
+# thought-provoking
+# coming of age
+#
+#
+# guffnoun:
+# tour de force
+# polemic
+# return to form
+# drama
+# comedy
+# romance
+# mediation
+#
+# director adjective:
+# enfant terrible
+# visionary
+# veteran
+# first-time
+# sophomore
+#
+#
+# guffperbole:
+# reinvigorate audiences young and old alike
+# make you question your very sanity
+# change not only your life, but those of all around you
+# make u think - forever
+# be sure to remain on the lips of the city's cognoscenti for decades
+# delight you with its acerbic wit
+#
+# sites:
+# picturehouse venues
+# car parks
+# school halls
+# imax theatres
+#
+# rating:
+# U
+# PG
+# 12A
+# 15
+# 18
+# X
+# XXX
+#
+# ratestrength:
+# mild
+# extreme
+# partial
+# occasional
+# constant
+#
+# ratecontent:
+# nudity
+# peril
+# bad language
+# blasphemy
+# negativity
+# showoffery
+# threat
+# violence
+# injury detail
+# drug use
+# gore
+# material likely to offend
+# risky immitable behaviour
+
+
+rateContent = ["nudity", "peril", "bad language", "blasphemy". "negativity",
+  "showoffery", "threat", "violence", "injury detail", "drug use", "gore",
+  "material likely to offend", "risky immitable behaviour"]
+
+print rateContent.sample
