3d2482dc6f4604dc53f17d1d2eb62b938542e5ae
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2016-08-09 22:06:24 +0100

    alpha of site working

---
+++ app/syngen.rb
@@ -0,0 +1,113 @@
+require 'sinatra/base'
+
+class SynGen < Sinatra::Base
+
+  get '/' do
+    character = ["The King's janitor", "A schoolteacher", "A union baron",
+      "A stowaway", "A local artist in residence", "A down-on-their-luck lowlife"]
+
+    actor = ["Toby Young", "Toby Jones", "Michael Fassbender",
+      "Benedict Cumberbatch", "Alicia Vikander", "Scarlett Johannson"]
+
+    object = ["plunger", "cucumber", "mother", "head of state", "local mechanic",
+      "garden shed"]
+
+    surprisingThing = ["a portal to another dimension",
+      "the magical sword excalibur", "God incarnate",
+      "their boss' daughter, cursed by a witch"]
+
+    condition = ["Mayhem", "Chaos", "Hilarity", "A lovely time", "A horny mess"]
+
+    impresses = ["dazzles", "stuns", "shines", "effervesces", "delights",
+      "shocks the critics", "delivers"]
+
+    termStruggle = ["the size of his butte",
+      "the dissolution of the monastries under Henry VIII",
+      "the death of his goldfish",
+      "a car crash that left him fighting for his life",
+      "the enormity of existence"]
+
+    aMeetingAdjective = ["An innocent", "A surprise", "A lunch", "An unexpected"]
+
+    loveFollowing = ["a love story for the ages",
+      "a paen to the golden age of cinema", "a love letter to the city of New York"]
+
+    adjective = ["uplifting", "heartfelt", "thought-provoking", "mesmeric",
+      "eye-opening"]
+
+    profession = ["sculptor", "boxer", "actor", "general", "tinker", "tailor",
+      "soldier", "spy"]
+
+    timePeriod = ["18th Century", "1960s", "turn-of-the-century", "Victorian",
+      "Jazz-era"]
+
+    firstName = ["Vincent", "Jonny", "Christopher", "Sophie", "Selina"]
+
+    surname = ["Briggs", "Bloggs", "Wood", "Mackintosh", "Alcock"]
+
+    place = ["the industrial North West of England", "New York",
+      "Sub-Saharan Africa", "Western Australia", "French Guyana"]
+
+    event = ["Nicaraguan corn farming blight", "financial collapse",
+      "celebrity sex scandal", "sports victory"]
+
+    director = ["Michael Moore", "Morgan Spurlock", "Martin Scorcese",
+      "Clint Eastwood", "Quentin Tarantino", "Sophia Copolla", "Kathleen Bigelow",
+      "Lars von Trier"]
+
+    docHype = ["unflinching examination of the human condition",
+      "testimony to the power of bearing witness",
+      "monument of modern understanding"]
+
+    guffAdjective = ["riproaring", "searing", "award-winning", "visceral",
+        "Palm d'Or-nominated", "whipsmart", "quirky", "thought-provoking",
+      "coming of age"]
+
+    guffNoun = ["tour de force", "polemic", "return to form", "drama", "comedy",
+      "romance", "meditation"]
+
+    directorAdjective = ["enfant terrible", "visionary", "veteran", "first-time",
+       "sophomore"]
+
+    guffperbole = ["reinvigorate audiences young and old alike",
+      "make you question your very sanity",
+      "change not only your life, but those of all around you",
+      "make u think - forever",
+      "be sure to remain on the lips of the city's cognoscenti for decades",
+      "delight you with its acerbic wit"]
+
+
+    ratingCode = ["U", "PG", "12A", "15", "18", "X", "XXX"]
+
+    rateStrength = ["mild", "extreme", "partial", "occasional", "constant"]
+
+    rateContent = ["nudity", "peril", "bad language", "blasphemy", "negativity",
+      "showoffery", "threat", "violence", "injury detail", "drug use", "gore",
+      "material likely to offend", "risky immitable behaviour"]
+
+    lifeTurnedUpsideDown = "#{character.sample} (#{actor.sample}) has their life turned upsidedown when they discover that their #{object.sample} is #{surprisingThing.sample}.  #{condition.sample} ensues."
+
+    emotionalJourney = "#{actor.sample} #{impresses.sample} in this portrayal of one man's emotional journey as he struggles to come to terms with the #{termStruggle.sample}."
+
+    aMeetsB = "#{aMeetingAdjective.sample} meeting between #{character.sample} (#{actor.sample}) and #{character.sample} (#{actor.sample}) quickly escalates into one of the greatest romances imaginable.  What follows is #{loveFollowing.sample}."
+
+    biopic = "The #{adjective.sample} story of the iconic #{timePeriod.sample} #{profession.sample} #{firstName.sample} #{surname.sample} (#{actor.sample}) offers a glimpse into the very soul of #{place.sample}."
+
+    documentary = "The untold story of the #{event.sample} that shook the world.  The film-makers were granted unprecedented access to create this #{docHype.sample}."
+
+    @plot = [lifeTurnedUpsideDown, emotionalJourney, aMeetsB, biopic, documentary].sample
+
+    @funnyBits = ["3 stars, some funny bits", "", "", "", "", "", "", "", "", ""].sample
+
+    sites = ["Picturehouse venues", "car parks", "school halls", "IMAX theatres"]
+
+    @presentation = "Presented in #{rand(8...180)}mm at selected #{sites.sample}"
+
+    @rating = "Rated #{ratingCode.sample} for #{rateStrength.sample} #{rateContent.sample}"
+
+    @length = "#{rand(3...607)} mins"
+
+    erb :index
+  end
+
+end
--- syngen.rb
+++ syngen.rb
@@ -1,245 +1,102 @@
-# PLOT
-#
-# 1. {life turned upsidedown} [character] ([actor]) has their life turned upsidedown when they discover that their [object] is [surprising thing].  [condition] ensues.
-#
-# 2. {emotional journey} [actor] [impresses] in this portrayal of one man's emotional journey as he struggles to come to terms with the [termstruggler].
-#
-# 3. {a meets b} [a meeting adjective] meeting between [character] ([actor]) and [character] ([actor]) quickly escalates into one of the greatest romances imaginable.  What follows is [love following].
-#
-# 4. {biopic} The [adjective] story of the iconic [time period] [profession] [firstname] [surname] ([actor]) offers a glimpse into the very soul of [place].
-#
-# 5. {documentary} The untold story of the [event] that shook the world.  The film makers were granted unprecedented access to create this [dochype].
-#
-#
-# GUFF
-#
-# 1. this [guffadjective] [guffnoun] from [director adjective] [director] will [guffperbole].
-#
-#
-#
-# TECHNICAL STUFF
-#
-# Presented in [random number between 8 and 180]mm at selected [sites]
-#
-# Rated [rating] for [ratestrength] [ratecontent].
-#
-# [random number between 3 and 607] minutes
-#
-# [one time in ten it says "3 stars, some funny bits"]
-#
-#
-# SUBBIES
-#
-# character:
-#
-# The King's janitor
-# A schoolteacher
-# A union baron
-# A stowaway
-# A local artist in residence
-# A down-on-their-luck lowlife
-#
-# actor:
-# Toby Young
-# Toby Jones
-# Michael Fassbender
-# Benedict Cumberbacth
-# Alicia Vikander
-# Scarlett Johannson
-#
-# object:
-# plunger
-# cucumber
-# mother
-# head of state
-# local mechanic
-# garden shed
-#
-# surprising thing:
-# a portal to another dimension
-# the magical sword excalibur
-# God incarnate
-# their boss' daughter, cursed by a witch
-#
-# condition:
-# Mayhem
-# Chaos
-# Hilarity
-# A lovely time
-# A horny mess
-#
-# impresses:
-# dazzles
-# stuns
-# shines
-# effervesces
-# delights
-# shocks the critics
-# delivers
-#
-# termstruggler:
-# the size of his butte
-# the dissolution of the monastries under Henry VIII
-# the death of his goldfish
-# a carcrash that left him fighting for his life.
-# the enormity of existence
-#
-# a meeting adjective:
-# An innocent
-# A surprise
-# A lunch
-# An unexpected
-#
-# love following:
-# a love story for the ages
-# a paen to the golden age of cinema
-# a love letter to the city of New York
-#
-# adjective:
-# uplifting
-# heartfelt
-# though-provoking
-# mesmeric
-# eye-opening
-#
-#
-# profession:
-# sculptor
-# boxer
-# actor
-# gener
-# tinker
-# tailor
-# soldier
-# spy
-#
-# time period:
-# 18th century
-# 1960s
-# turn of the century
-# victorian
-# jazz-era
-#
-# first name:
-# vincent
-# jonny
-# christopher
-# sophie
-# selina
-#
-# surname:
-# briggs
-# bloggs
-# wood
-# mackintosh
-# alcock
-#
-# place:
-# the industrial northwest of england
-# New York
-# Sub-saharan africa
-# Western Australia
-# French Guyana
-#
-# event:
-# Nicaraguan corn farming blight
-# financial collapse
-# celebrity sex scandal
-# sports victory
-#
-# director:
-# Michael Moore
-# Morgan Spurlock
-# Martin Scorcese
-# Clint Eastwood
-# Quentine Tarantino
-# Sophia Copolla
-# Kathleen Bigelow
-# Lars von Trier
-#
-#
-# dochype:
-# unflinching examination of the human condition
-# testimony to the power of bearing witness
-# monument of modern understanding
-#
-# guffadjective:
-# riproaring
-# searing
-# award winning
-# visceral
-# palm d'or nominated
-# whipsmart
-# quirky
-# thought-provoking
-# coming of age
-#
-#
-# guffnoun:
-# tour de force
-# polemic
-# return to form
-# drama
-# comedy
-# romance
-# mediation
-#
-# director adjective:
-# enfant terrible
-# visionary
-# veteran
-# first-time
-# sophomore
-#
-#
-# guffperbole:
-# reinvigorate audiences young and old alike
-# make you question your very sanity
-# change not only your life, but those of all around you
-# make u think - forever
-# be sure to remain on the lips of the city's cognoscenti for decades
-# delight you with its acerbic wit
-#
-# sites:
-# picturehouse venues
-# car parks
-# school halls
-# imax theatres
-#
-# rating:
-# U
-# PG
-# 12A
-# 15
-# 18
-# X
-# XXX
-#
-# ratestrength:
-# mild
-# extreme
-# partial
-# occasional
-# constant
-#
-# ratecontent:
-# nudity
-# peril
-# bad language
-# blasphemy
-# negativity
-# showoffery
-# threat
-# violence
-# injury detail
-# drug use
-# gore
-# material likely to offend
-# risky immitable behaviour
+character = ["The King's janitor", "A schoolteacher", "A union baron",
+  "A stowaway", "A local artist in residence", "A down-on-their-luck lowlife"]
+
+actor = ["Toby Young", "Toby Jones", "Michael Fassbender",
+  "Benedict Cumberbatch", "Alicia Vikander", "Scarlett Johannson"]
+
+object = ["plunger", "cucumber", "mother", "head of state", "local mechanic",
+  "garden shed"]
+
+surprisingThing = ["a portal to another dimension",
+  "the magical sword excalibur", "God incarnate",
+  "their boss' daughter, cursed by a witch"]
+
+condition = ["Mayhem", "Chaos", "Hilarity", "A lovely time", "A horny mess"]
+
+impresses = ["dazzles", "stuns", "shines", "effervesces", "delights",
+  "shocks the critics", "delivers"]
+
+termStruggle = ["the size of his butte",
+  "the dissolution of the monastries under Henry VIII",
+  "the death of his goldfish",
+  "a car crash that left him fighting for his life",
+  "the enormity of existence"]
+
+aMeetingAdjective = ["An innocent", "A surprise", "A lunch", "An unexpected"]
+
+loveFollowing = ["a love story for the ages",
+  "a paen to the golden age of cinema", "a love letter to the city of New York"]
+
+adjective = ["uplifting", "heartfelt", "thought-provoking", "mesmeric",
+  "eye-opening"]
+
+profession = ["sculptor", "boxer", "actor", "general", "tinker", "tailor",
+  "soldier", "spy"]
+
+timePeriod = ["18th Century", "1960s", "turn-of-the-century", "Victorian",
+  "Jazz-era"]
+
+firstName = ["Vincent", "Jonny", "Christopher", "Sophie", "Selina"]
+
+surname = ["Briggs", "Bloggs", "Wood", "Mackintosh", "Alcock"]
+
+place = ["the industrial North West of England", "New York",
+  "Sub-Saharan Africa", "Western Australia", "French Guyana"]
+
+event = ["Nicaraguan corn farming blight", "financial collapse",
+  "celebrity sex scandal", "sports victory"]
+
+director = ["Michael Moore", "Morgan Spurlock", "Martin Scorcese",
+  "Clint Eastwood", "Quentin Tarantino", "Sophia Copolla", "Kathleen Bigelow",
+  "Lars von Trier"]
+
+docHype = ["unflinching examination of the human condition",
+  "testimony to the power of bearing witness",
+  "monument of modern understanding"]
+
+guffAdjective = ["riproaring", "searing", "award-winning", "visceral",
+    "Palm d'Or-nominated", "whipsmart", "quirky", "thought-provoking",
+  "coming of age"]
+
+guffNoun = ["tour de force", "polemic", "return to form", "drama", "comedy",
+  "romance", "meditation"]
+
+directorAdjective = ["enfant terrible", "visionary", "veteran", "first-time",
+   "sophomore"]
+
+guffperbole = ["reinvigorate audiences young and old alike",
+  "make you question your very sanity",
+  "change not only your life, but those of all around you",
+  "make u think - forever",
+  "be sure to remain on the lips of the city's cognoscenti for decades",
+  "delight you with its acerbic wit"]
 
 
-rateContent = ["nudity", "peril", "bad language", "blasphemy". "negativity",
+ratingCode = ["U", "PG", "12A", "15", "18", "X", "XXX"]
+
+rateStrength = ["mild", "extreme", "partial", "occasional", "constant"]
+
+rateContent = ["nudity", "peril", "bad language", "blasphemy", "negativity",
   "showoffery", "threat", "violence", "injury detail", "drug use", "gore",
   "material likely to offend", "risky immitable behaviour"]
 
-print rateContent.sample
+lifeTurnedUpsideDown = "#{character.sample} (#{actor.sample}) has their life turned upsidedown when they discover that their #{object.sample} is #{surprisingThing.sample}.  #{condition.sample} ensues."
+
+emotionalJourney = "#{actor.sample} #{impresses.sample} in this portrayal of one man's emotional journey as he struggles to come to terms with the #{termStruggle.sample}."
+
+aMeetsB = "#{aMeetingAdjective.sample} meeting between #{character.sample} (#{actor.sample}) and #{character.sample} (#{actor.sample}) quickly escalates into one of the greatest romances imaginable.  What follows is #{loveFollowing.sample}."
+
+biopic = "The #{adjective.sample} story of the iconic #{timePeriod.sample} #{profession.sample} #{firstName.sample} #{surname.sample} (#{actor.sample}) offers a glimpse into the very soul of #{place.sample}."
+
+documentary = "The untold story of the #{event.sample} that shook the world.  The film-makers were granted unprecedented access to create this #{docHype.sample}."
+
+plot = [lifeTurnedUpsideDown, emotionalJourney, aMeetsB, biopic, documentary]
+
+funnyBits = ["3 stars, some funny bits", "", "", "", "", "", "", "", "", ""]
+
+sites = ["picturehouse venues", "car parks", "school halls", "IMAX theatres"]
+presentation = "Presented in #{rand(8...180)}[random number between 8 and 180]mm at selected #{sites.sample}"
+
+rating = "Rated #{ratingCode.sample} for #{rateStrength.sample} #{rateContent.sample}"
+
+length = "#{rand(3...607)} mins"
